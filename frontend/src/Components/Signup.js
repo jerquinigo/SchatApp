7aff8c7118f922655a735d105f09d5a0a03d9924
Jonathan.e <jonathanerquinigo@pursuit.org>
2019-11-26 13:59:34 -0500

    updating project directory

--- frontend/src/Components/Signup.js
+++ frontend/src/Components/Signup.js
@@ -3,6 +3,30 @@ import React, {Component} from "react"
 class Signup extends Component{
     constructor(){
         super()
+        this.state = {
+            username = "",
+            password = "",
+            isSubmitted = false
+        }
+
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+
+
+    displaySignupForm = () => {
+        return(
+        <div>
+        <form>
+            <input type="text" value="username" />
+        </form>
+        </div>
+        )
     }
 
 
